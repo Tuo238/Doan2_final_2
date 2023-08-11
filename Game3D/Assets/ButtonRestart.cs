6c2c25e03335668680da03fdd5d9068e16d26772
nankiee <trannhatnam912@gmail.com>
2023-08-11 20:16:36 +0700

    Update CheckPoint -Menu

---
+++ Game3D/Assets/ButtonRestart.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathScene : MonoBehaviour
+{
+    public Transform[] checkpoints; // Mảng lưu trữ các checkpoint
+    private Vector3 spawnPoint; // Vị trí xuất hiện
+
+    void Start()
+    {
+        Button button = GetComponent<Button>(); // Lấy tham chiếu đến component Button
+        button.onClick.AddListener(ReturnToCheckPoint); // Đăng ký hàm ReturnToCheckPoint khi button được nhấn
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void ReturnToCheckPoint()
+    {
+        // Kiểm tra xem người chơi đã tiếp xúc với checkpoint chưa
+        for (int i = 0; i < checkpoints.Length; i++)
+        {
+            if (transform.position == checkpoints[i].position)
+            {
+                // Cập nhật vị trí xuất hiện khi tiếp xúc với checkpoint
+                spawnPoint = checkpoints[i].position;
+                // Không cần hủy checkpoint ở đây
+                break;
+            }
+        }
+
+        // Đặt vị trí của người chơi thành vị trí checkpoint
+        transform.position = spawnPoint;
+    }
+}
--- Game3D/Assets/Script/DeathScreen.cs
+++ Game3D/Assets/Script/DeathScreen.cs
@@ -1,24 +1,50 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DeathScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform[] checkpoints; // Mảng lưu trữ các checkpoint
+    private Vector3 spawnPoint; // Vị trí xuất hiện
+
     void Start()
     {
-        
+        Button button = GetComponent<Button>(); // Lấy tham chiếu đến component Button
+        button.onClick.AddListener(ReturnToCheckPoint); // Đăng ký hàm ReturnToCheckPoint khi button được nhấn
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void loadMainMenu()
+    private void ReturnToCheckPoint()
     {
-        SceneManager.LoadScene("MainMenu");
+        // Kiểm tra xem người chơi đã tiếp xúc với checkpoint chưa
+        for (int i = 0; i < checkpoints.Length; i++)
+        {
+            if (transform.position == checkpoints[i].position)
+            {
+                // Cập nhật vị trí xuất hiện khi tiếp xúc với checkpoint
+                spawnPoint = checkpoints[i].position;
+                // Không cần hủy checkpoint ở đây
+                break;
+            }
+        }
+
+        // Đặt vị trí của người chơi thành vị trí checkpoint
+        transform.position = spawnPoint;
+
+        // Tạo Empty GameObject "DeathScreen" và đặt vị trí tương tự như vị trí hiện tại của người chơi
+        GameObject deathScreenObject = new GameObject("DeathScreen");
+        deathScreenObject.transform.position = transform.position;
     }
+
+
+    //public void loadMainMenu()
+    //{
+    //    SceneManager.LoadScene("MainMenu");
+    //}
 }
